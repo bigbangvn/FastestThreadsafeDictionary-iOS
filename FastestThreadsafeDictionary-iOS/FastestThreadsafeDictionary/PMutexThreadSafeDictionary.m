6483099786f2c71fe709026237372ad3e0f06039
BangNT <bangntr@gmail.com>
2016-09-01 18:37:01 +0700

    add an implementation use pthread mutex to compare

---
+++ FastestThreadsafeDictionary-iOS/FastestThreadsafeDictionary/PMutexThreadSafeDictionary.m
@@ -0,0 +1,119 @@
+//
+//  TBThreadSafeMutableDictionary.m
+//  TestLockless-iOS
+//
+//  Created by trongbangvp@gmail.com on 9/1/16.
+//  Copyright © 2016 trongbangvp@gmail.com. All rights reserved.
+//
+#import <pthread/pthread.h>
+#import "PMutexThreadSafeDictionary.h"
+
+@interface PMutexThreadSafeDictionary()
+{
+    pthread_mutex_t _mutex;
+}
+@property(atomic, strong) NSMutableDictionary* dic;
+@end
+
+@implementation PMutexThreadSafeDictionary
+
+-(id) init
+{
+    if(self = [super init])
+    {
+        pthread_mutex_init(&_mutex, NULL);
+        _dic = [NSMutableDictionary new];
+    }
+    return self;
+}
+
+#pragma mark - Read operation
+//Read operation is lockless, just checking lock-write
+
+-(NSUInteger)count
+{
+    pthread_mutex_lock(&_mutex);
+    
+    NSUInteger n = self.dic.count;
+    
+    pthread_mutex_unlock(&_mutex);
+    return n;
+}
+
+-(id) objectForKey:(id)aKey
+{
+    pthread_mutex_lock(&_mutex);
+    
+    id val = [self.dic objectForKey:aKey];
+    
+    pthread_mutex_unlock(&_mutex);
+    return val;
+}
+
+- (NSEnumerator*)keyEnumerator
+{
+    pthread_mutex_lock(&_mutex);
+    
+    id val = [self.dic keyEnumerator];
+    
+    pthread_mutex_unlock(&_mutex);
+    return val;
+}
+- (NSArray*)allKeys
+{
+    pthread_mutex_lock(&_mutex);
+    
+    id val = [self.dic allKeys];
+    
+    pthread_mutex_unlock(&_mutex);
+    return val;
+}
+- (NSArray*)allValues
+{
+    pthread_mutex_lock(&_mutex);
+    
+    id val = [self.dic allValues];
+    
+    pthread_mutex_unlock(&_mutex);
+    return val;
+}
+
+#pragma mark - Write operation
+-(void) setObject:(id)anObject forKey:(id<NSCopying>)aKey
+{
+    pthread_mutex_lock(&_mutex);
+    
+    [self.dic setObject:anObject forKey:aKey];
+    
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (void)removeObjectForKey:(id)aKey
+{
+    pthread_mutex_lock(&_mutex);
+    
+    [self.dic removeObjectForKey:aKey];
+    
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (void)removeObjectsForKeys:(NSArray *)keyArray
+{
+    pthread_mutex_lock(&_mutex);
+    
+    [self.dic removeObjectsForKeys:keyArray];
+    
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (void)removeAllObjects
+{
+    pthread_mutex_lock(&_mutex);
+    
+    [self.dic removeAllObjects];
+    
+    pthread_mutex_unlock(&_mutex);
+}
+
+@end
+
